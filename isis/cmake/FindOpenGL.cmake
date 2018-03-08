512d4ec537084632de3166d1992cf6261780b17d
Christopher Ryan Combs Jr <ryancombsjr@gmail.com>
2018-03-08 10:00:12 -0700

    Update FindOpenGL.cmake

--- isis/cmake/FindOpenGL.cmake
+++ isis/cmake/FindOpenGL.cmake
@@ -17,5 +17,9 @@ find_library(GL_LIBRARY
 find_library(GLU_LIBRARY
   NAMES GLU
 )
+message("==================")
+message(OPENGL_INCLUDE_DIR)
+message(GL_LIBRARY)
+message(GLU_LIBRARY)
 
 get_filename_component(OPENGL_INCLUDE_DIR "${OPENGL_INCLUDE_DIR}" DIRECTORY)
