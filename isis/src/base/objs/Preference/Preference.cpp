4bcc5af8b00c42ba5f5ae8ee0cfc4a53572e5c16
Christopher Ryan Combs Jr <ryancombsjr@gmail.com>
2018-03-09 16:49:08 -0700

    Debugging TestPreferences for docker

--- isis/src/base/objs/Preference/Preference.cpp
+++ isis/src/base/objs/Preference/Preference.cpp
@@ -115,11 +115,11 @@ namespace Isis {
 
       // If its a unitTest then load with the unitTest preference file
       if(unitTest) {
-        p_preference->Load("/scratch/workspace/ISIS3_cmake/build/TestPreferences");
+        p_preference->Load("$ISISROOT/TestPreferences");
       }
       // Otherwise load the Isis system and personal preferences.
       else {
-        p_preference->Load("/scratch/workspace/ISIS3_cmake/build/IsisPreferences");
+        p_preference->Load("$ISISROOT/IsisPreferences");
 
         Isis::FileName userPref("$HOME/.Isis/IsisPreferences");
         if(userPref.fileExists()) {
