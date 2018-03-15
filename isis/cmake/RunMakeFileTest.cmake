eda8fb621aec709773bf8110862aea14ce68c855
Christopher Ryan Combs Jr <ryancombsjr@gmail.com>
2018-03-15 11:49:01 -0700

    Update RunMakeFileTest.cmake

--- isis/cmake/RunMakeFileTest.cmake
+++ isis/cmake/RunMakeFileTest.cmake
@@ -51,7 +51,7 @@ function(run_app_makefile_test makefile inputFolder outputFolder truthFolder bin
 #   execute_process(COMMAND rm -f ${logFile})
 
   execute_process(COMMAND make test WORKING_DIRECTORY ${sourceFolder} OUTPUT_VARIABLE result)
-
+  message("result: ${result}")
   if (result MATCHES "OK")
       set(failed "OFF")
   else()
